ca36c4a60d153ec0ec5d190564f06aab640260cd
Mehmet Sever <mehmetsever@Mehmets-MacBook-Pro.local>
2020-10-31 11:25:56 -0400

    First commit

---
+++ Documents/capstone/shell_scripts/prom_node.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+cat > /etc/prometheus/prometheus.yml <<-"EOF"
+global:
+  scrape_interval: 15s
+
+scrape_configs:
+  - job_name: 'prometheus'
+    scrape_interval: 5s
+    static_configs:
+      - targets: ['localhost:9090']
+  - job_name: 'node_exporter'
+    scrape_interval: 5s
+    static_configs:
+      - targets: ['localhost:9100']
+EOF
+
+sudo systemctl restart prometheus
+sudo systemctl status prometheus
